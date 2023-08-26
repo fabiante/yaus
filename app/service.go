52c455de496fd3f71afdaf0187b6276bb67f0480
fabiante <fabiante@users.noreply.github.com>
2023-08-26 17:03:17 +0200

    Produce shorter urls and refactor test assertion

--- app/service.go
+++ app/service.go
@@ -1,5 +1,7 @@
 package app
 
+import "github.com/google/uuid"
+
 type Service struct {
 }
 
@@ -8,5 +10,14 @@ func NewService() *Service {
 }
 
 func (s *Service) ShortenURL(input string) (string, error) {
-	return input, nil
+	// todo: validate input - users should not store random data - write test first
+
+	// todo: validate input - length should not exceed X - write test first
+
+	// generate a random id for this url
+	id := uuid.New()
+
+	// todo: actually store ID + input URL so that they can be resolved later - write test first
+
+	return id.String(), nil
 }
--- bdd/specs/tests.go
+++ bdd/specs/tests.go
@@ -1,17 +1,18 @@
 package specs
 
 import (
-	"github.com/carlmjohnson/be"
+	"github.com/stretchr/testify/require"
 	"testing"
 )
 
 func TestAll(t *testing.T, service Service) {
 	t.Run("feature: shortening a link link", func(t *testing.T) {
-		t.Run("given a valid URL", func(t *testing.T) {
+		t.Run("given a valid URL will produce a shorter URL", func(t *testing.T) {
 			input := "https://en.wikipedia.org/wiki/Go_(programming_language)#Types"
 			url, err := service.ShortenURL(input)
-			be.NilErr(t, err)
-			be.Unequal(t, "", url)
+			require.NoError(t, err)
+			require.NotEmpty(t, url)
+			require.Less(t, len(url), len(input)) // produce a shorter url
 		})
 	})
 }
