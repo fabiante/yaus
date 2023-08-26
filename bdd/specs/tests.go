8f04a0711b9fa844155705d0695e15d0c60f6ea6
fabiante <fabiante@users.noreply.github.com>
2023-08-26 17:19:58 +0200

    Add tests for input length validation

--- bdd/specs/tests.go
+++ bdd/specs/tests.go
@@ -11,23 +11,44 @@ func TestAll(t *testing.T, service Service) {
 		t.Run("given a valid URL will produce a shorter URL", func(t *testing.T) {
 			input := "https://en.wikipedia.org/wiki/Go_(programming_language)#Types"
 			url, err := service.ShortenURL(input)
-			require.NoError(t, err)
-			require.NotEmpty(t, url)
-			require.Less(t, len(url), len(input)) // produce a shorter url
+			requireShortenedURL(t, err, url, input)
 		})
 
 		t.Run("given invalid input will error out", func(t *testing.T) {
-			invalid := []string{
+			data := []string{
 				"",
 				"not a url",
 			}
 
-			for i, input := range invalid {
+			for i, input := range data {
 				t.Run(fmt.Sprintf("data[%d]", i), func(t *testing.T) {
 					_, err := service.ShortenURL(input)
 					require.ErrorContains(t, err, "invalid url")
 				})
 			}
+
+			// These tests ensure that there is some kind of input size limit.
+			// This must not exactly be 512 bytes or characters. It should just generally
+			// prevent the service from being used as a database.
+			t.Run("validates length", func(t *testing.T) {
+				t.Run("allows 512 bytes", func(t *testing.T) {
+					input := "http://8123456781234567812345678123456781234567812345678123456781234567812345678123456781234567812345678123456781234567812345678123456781234567812345678123456781234567812345678123456781234567812345678123456781234567812345678123456781234567812345678123456781234567812345678123456781234567812345678123456781234567812345678123456781234567812345678123456781234567812345678123456781234567812345678123456781234567812345678123456781234567812345678123456781234567812345678123456781234567812345678123456781234567812345678"
+					url, err := service.ShortenURL(input)
+					requireShortenedURL(t, err, url, input)
+				})
+
+				t.Run("rejects 513 bytes", func(t *testing.T) {
+					input := "http://78123456781234567812345678123456781234567812345678123456781234567812345678123456781234567812345678123456781234567812345678123456781234567812345678123456781234567812345678123456781234567812345678123456781234567812345678123456781234567812345678123456781234567812345678123456781234567812345678123456781234567812345678123456781234567812345678123456781234567812345678123456781234567812345678123456781234567812345678123456781234567812345678123456781234567812345678123456781234567812345678123456781234567812345678"
+					_, err := service.ShortenURL(input)
+					require.ErrorContains(t, err, "invalid url")
+				})
+			})
 		})
 	})
+}
+
+func requireShortenedURL(t *testing.T, err error, url string, input string) {
+	require.NoError(t, err)
+	require.NotEmpty(t, url)
+	require.Less(t, len(url), len(input)) // produce a shorter url
 }
