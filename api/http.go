49ff6dfefe73143d96f6654f3faf2c33abc7f090
fabiante <fabiante@users.noreply.github.com>
2023-08-27 11:07:46 +0200

    Implement resolve endpoint in http api

--- api/http.go
+++ api/http.go
@@ -2,12 +2,14 @@ package api
 
 import (
 	"errors"
+	"fmt"
 	"github.com/fabiante/yaus/app"
 	"github.com/gin-gonic/gin"
+	"net/http"
 )
 
 func SetupHTTPServer(api *gin.Engine, service *app.Service) *gin.Engine {
-	api.POST("shorten", func(ctx *gin.Context) {
+	api.POST("/shorten", func(ctx *gin.Context) {
 		var url string
 		err := ctx.BindJSON(&url)
 		if err != nil {
@@ -27,6 +29,27 @@ func SetupHTTPServer(api *gin.Engine, service *app.Service) *gin.Engine {
 		}
 
 		ctx.JSON(200, shortened)
+	})
+
+	api.GET("/s/:short", func(ctx *gin.Context) {
+		short := ctx.Param("short")
+		if short == "" {
+			_ = ctx.AbortWithError(400, fmt.Errorf("no short to resolve"))
+			return
+		}
+
+		shortened, err := service.Resolve(short)
+		if err != nil {
+			if errors.Is(err, app.ErrInvalidUrl) {
+				_ = ctx.AbortWithError(400, err)
+				return
+			} else {
+				_ = ctx.AbortWithError(500, err)
+				return
+			}
+		}
+
+		ctx.Redirect(http.StatusPermanentRedirect, shortened)
 	})
 
 	return api
